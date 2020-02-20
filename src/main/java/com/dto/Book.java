699c72adb1a9f67439c009de768f9f57b30ec3ad
Vladyslav Nikolaiev <vladyslav.nikolaiev@nordstrom.com>
2020-02-20 20:00:54 +0200

    DTOs added

---
+++ src/main/java/com/dto/Book.java
@@ -0,0 +1,10 @@
+package com.dto;
+
+import lombok.Data;
+
+@Data
+public class Book {
+
+    int id;
+    int score;
+}
--- src/main/java/com/dto/Library.java
+++ src/main/java/com/dto/Library.java
@@ -1,5 +1,18 @@
 package com.dto;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class Library {
+
+    int number;
+    int booksCount;
+    int signUpDays;
+    int bookPerDay;
+
+    List<Book> books;
+
 
 }
