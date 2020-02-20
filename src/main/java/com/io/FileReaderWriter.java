4a3e237d636ef9926313e8b9a97b122683765e97
Vladyslav Nikolaiev <vladyslav.nikolaiev@nordstrom.com>
2020-02-20 19:43:40 +0200

    Com added

---
+++ src/main/java/com/io/FileReaderWriter.java
@@ -0,0 +1,23 @@
+package com.io;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class FileReaderWriter {
+
+    public static List<String> readFile(String path) throws IOException {
+        File file = new File("demo.txt");
+
+        try {
+            return FileUtils.readLines(file, StandardCharsets.UTF_8);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+}
